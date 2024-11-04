4383bc297a39207449a858feb52bc817c39ca3d1
Gokhan Gunduz <gokhangndz@gmail.com>
2024-11-04 16:42:51 +0300

    format corrections

--- docker-module-containerlab/main.tf
+++ docker-module-containerlab/main.tf
@@ -1,7 +1,7 @@
 # tofu file for ubuntu container with ssh access to host computer with public key
 
 provider "docker" {
-  host = unix:///var/run/docker.sock"
+  host = "unix:///var/run/docker.sock"
 }
 
 locals {
@@ -13,34 +13,33 @@ variable "num_containers" {
 }
 
 resource "docker_image" "ubuntu_ssh" {
-  name = "ubuntu:latest"
+  name         = "ubuntu:latest"
   keep_locally = true
 }
 
-resource "docker_container" "ubuntu_container" {
+resource "docker_image" "ubuntu_container" {
   count = var.num_containers
-  name = "ubuntu_ssh_${count.index}
+  name  = "ubuntu_ssh_${count.index}"
   image = docker_image.ubuntu_ssh.latest
-}
 
-ports {
-  internal = 22
-  external = 2200 + count.index
-}
-
-provisioner "remote-exec" {
-  inline = [
-    "apt-get update",                   
-    "apt-get install -y openssh-server",
-    "mkdir -p /root/.ssh/",
-    "echo '${local.ssh_public_key}' >> /root/.ssh/authorized_keys",
-    "chmod 600 /root/.ssh/authorized_keys",
-    "systemctl enable --now ssh.service"
-  ]
-  
-  connection {
-    type = "docker"
-    user = "root"
-    }
+  ports {
+    internal = 22
+    external = 2200 + count.index
   }
 
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get update",
+      "apt-get install -y openssh-server",
+      "mkdir -p /root/.ssh/",
+      "echo '${local.ssh_public_key}' >> /root/.ssh/authorized_keys",
+      "chmod 600 /root/.ssh/authorized_keys",
+      "systemctl enable --now ssh.service"
+    ]
+
+    connection {
+      type = "docker"
+      user = "root"
+    }
+  }
+}

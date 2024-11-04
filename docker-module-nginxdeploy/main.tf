79079696edb5f9b312af29b52d57d1e39cb7d61f
Gokhan Gunduz <gokhangndz@gmail.com>
2024-11-04 14:16:02 +0300

    dockermodule

---
+++ docker-module-nginxdeploy/main.tf
@@ -0,0 +1,13 @@
+provider "docker" {}
+resource "docker_image" "nginx" {
+    name = "nginx:latest"
+    keep_locally = false
+}
+resource "docker_container" "nginx" {
+    image = docker_image.nginx.image_id
+    name = "docker-module-nginxdeploy"
+    ports {
+        internal = 80
+        external = 8000
+    }
+}
